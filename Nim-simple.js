12f00201891a8fa2dfae57f6b4ea344592c4ae03
aoneildunne <54771746+aoneildunne@users.noreply.github.com>
2020-01-24 14:34:04 -0500

    Create Nim-simple.js

---
+++ Nim-simple.js
@@ -0,0 +1,45 @@
+/* Nim Super Simple */
+
+/* Globals 
+count = 0
+ * count = integer, winner = String
+ */ 
+ count = 0;
+ turn = 0;
+ winner = "null";
+ /* Main Code 
+  * loop while count <21
+  * if count < 21 call cpuTurn, adding to count and alerting count. Set winner as player. 
+  * if count < 21 call playerTurn, adding to count and alerting count. Set winner as CPU.
+  * end loop
+  * alert winner
+  */
+  while(count < 21){
+	  if(count < 21){
+		  cpuTurn();
+		  
+	  }
+  }
+  
+/* playerTurn
+ * set winner as CPU
+ * prompt for turn (1-3)
+ * @param none
+ * @return turn (integer)
+ */
+function userturn(){
+	winner = cpu;
+	parseint
+}
+/* cpuTurn
+ * set winner as player
+ * set turn as random number 1-3
+ * alert CPU's turn
+ * return turn
+ */
+ function cpuTurn(){
+	 winner = player
+	 turn = Math.floor(Math.random()*3)+1;
+	 alert(turn);
+	 return turn;
+ }
