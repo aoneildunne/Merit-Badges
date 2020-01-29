fe8c24c0f68896879747f28429685a488d997a32
aoneildunne <54771746+aoneildunne@users.noreply.github.com>
2020-01-29 14:17:32 -0500

    Update nim-simple-justcode.js

--- nim-simple-justcode.js
+++ nim-simple-justcode.js
@@ -2,25 +2,25 @@ count = 0;
 turn = 0;
 winner = null;
 while(count < 21){
-	 if(count < 21){
-		 cpuTurn();
-		 }
    if(count < 21){
-     playerTurn();
+	cpuTurn();
    }
- }
- alert(" winner is " +winner+ " ");
+   if(count < 21){
+       playerTurn();
+   }
+}
+alert(" winner is " +winner+ " ");
 function playerTurn(){
-	winner = "cpu";
-	turn = parseInt(prompt("how many do you count"));
+  winner = "cpu";
+  turn = parseInt(prompt("how many do you count"));
   count+=turn;
   alert("count is now " +count+ " .");
   return turn;
 }
 function cpuTurn(){
-	 winner = "player";
-	 turn = Math.floor(Math.random()*3)+1;
+   winner = "player";
+   turn = Math.floor(Math.random()*3)+1;
    count+=turn;
-	 alert("computer counts " +turn+ " and count is now " +count+ " .");
+   alert("computer counts " +turn+ " and count is now " +count+ " .");
    return turn;
  }
